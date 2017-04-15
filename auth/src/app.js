e33d257f92e49a2399fc4e410560d9fdffe01ea2
Božidar Labaš <bozidar.labas@fiveminutes.eu>
2017-04-15 21:04:12 +0200

    Added eslint module and finished LoginForm

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,28 +1,29 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
-import { Firebase } from 'firebase';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
 
 export default class App extends Component {
 
     componentWillMount() {
         // Firebase initialization
         // Connect to specific bucket of data on server
-        firebase.initializeApp({
+         firebase.initializeApp({
             apiKey: 'AIzaSyCi1QtRkSaXLjY-fBs66EzNBeEcsYDnNc8',
             authDomain: 'auth-11b88.firebaseapp.com',
             databaseURL: 'https://auth-11b88.firebaseio.com',
             projectId: 'auth-11b88',
             storageBucket: 'auth-11b88.appspot.com',
             messagingSenderId: '407947763033'
-        })
+        });
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <Text>App</Text>
+                <LoginForm />
             </View>
         );
     };
--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,10 +1,38 @@
-import React from 'react-native';
-import { View } from 'react';
+import React, { Component } from 'react';
+import { Card, CardSection, Button, Input } from './common';
 
 export default class LoginForm extends Component {
+
+    state = {email: '', password: '' }; 
+
     render() {
         return (
-            <View />
-        )
-    }
+            <Card>
+                <CardSection>
+                    <Input 
+                        placeholder="user@gmail.com"
+                        label="Email"
+                        value={this.state.email}
+                        onChangeText={email => this.setState({ email })}
+                    />
+                </CardSection>
+
+                <CardSection>
+                    <Input 
+                        secureTextEntry //equal to secureTextEntry={true}
+                        placeholder="password"
+                        label="Password"
+                        value={this.state.password}
+                        onChangeText={password => this.setState({ password })}
+                    />
+                </CardSection>
+                
+                 <CardSection>
+                     <Button>
+                         Login
+                     </Button>
+                 </CardSection>
+            </Card>
+        );
+    };
 }
