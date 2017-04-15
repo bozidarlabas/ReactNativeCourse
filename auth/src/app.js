8a3c2aef7a0d84a12ab94c3406c7a8cebde6f7bc
Božidar Labaš <bozidar.labas@fiveminutes.eu>
2017-04-15 18:15:56 +0200

    Created file LoginForm.js

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,8 +1,23 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import { Firebase } from 'firebase';
 import { Header } from './components/common';
 
 export default class App extends Component {
+
+    componentWillMount() {
+        // Firebase initialization
+        // Connect to specific bucket of data on server
+        firebase.initializeApp({
+            apiKey: 'AIzaSyCi1QtRkSaXLjY-fBs66EzNBeEcsYDnNc8',
+            authDomain: 'auth-11b88.firebaseapp.com',
+            databaseURL: 'https://auth-11b88.firebaseio.com',
+            projectId: 'auth-11b88',
+            storageBucket: 'auth-11b88.appspot.com',
+            messagingSenderId: '407947763033'
+        })
+    }
+
     render() {
         return (
             <View>
@@ -12,3 +27,4 @@ export default class App extends Component {
         );
     };
 }
+
---
+++ auth/src/components/LoginForm.js
@@ -0,0 +1,10 @@
+import React from 'react-native';
+import { View } from 'react';
+
+export default class LoginForm extends Component {
+    render() {
+        return (
+            <View />
+        )
+    }
+}
