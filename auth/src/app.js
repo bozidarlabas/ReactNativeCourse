046e169f16c8d89baa9e275cd7c77aec0f0ee041
Božidar Labaš <bozidar.labas@fiveminutes.eu>
2017-04-16 19:00:39 +0200

    Finished authentiation

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from './components/common';
-import LoginForm from './components/LoginForm'
+import { Header, Button, Card, CardSection, Spinner } from './components/common';
+import LoginForm from './components/LoginForm';
 
 export default class App extends Component {
+
+    state = { loggedIn: null };
 
     componentWillMount() {
         // Firebase initialization
@@ -17,15 +19,39 @@ export default class App extends Component {
             storageBucket: 'auth-11b88.appspot.com',
             messagingSenderId: '407947763033'
         });
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user) {
+                this.setState({ loggedIn: true });
+            } else {
+                this.setState({ loggedIn: false });
+            }
+        });
+    }
+
+    renderContent() {
+        switch (this.state.loggedIn) {
+            case true:
+                return (    
+                         <CardSection>
+                            <Button onPress={() => firebase.auth().signOut()}>
+                                Log Lout
+                            </Button>
+                         </CardSection>
+            );
+            case false:
+                return <LoginForm />;
+            default:
+                return <Spinner size='large' />;
+        }
     }
 
     render() {
         return (
             <View>
                 <Header headerText="Authentication" />
-                <LoginForm />
+                {this.renderContent()}
             </View>
         );
-    };
+    }
 }
-
